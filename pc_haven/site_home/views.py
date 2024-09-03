6e4d8e0a4057d43b5c95392c73b34dd2ace36d86
Olonade Oreoluwa <praisesome@gmail.com>
2024-09-03 09:31:14 +0100

    updated frontend

--- pc_haven/site_home/views.py
+++ pc_haven/site_home/views.py
@@ -35,27 +35,27 @@ def signupPage(request):
     context = {
         "form": form 
     }
-    return render(request, "site_home/signupPage.html", context)
+    return render(request, "site_home/signup.html", context)
 
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect("home")
-    
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            redirect("home")
-        else:
-            messages.info(request, "Username or password is incorrect")
+    else:
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                redirect("home")
+            else:
+                messages.info(request, "Username or password is incorrect")
 
     context = {
 
     }
 
-    return render(request, "site_home/loginPage.html", context)
+    return render(request, "site_home/login.html", context)
 
 def logoutUser(request):
     logout(request)
